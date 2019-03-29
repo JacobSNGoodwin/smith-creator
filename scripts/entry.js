ccd5f271caaee0296e832e27bfe768c7f9d31626
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-03-28 20:03:01 -0700

    Get options data from touchstone file

--- scripts/entry.js
+++ scripts/entry.js
@@ -33,11 +33,22 @@ fileInput.addEventListener('change', event => {
   reader.readAsText(file)
 })
 
+const sparams = {}
 const reader = new FileReader()
-reader.onload = function(e) {
+reader.onload = () => {
   let text = reader.result
   const splitText = text.split('\n')
-  console.log(splitText)
-
-  // remove comment and unnecessary lines. Get plot type
+  splitText.forEach(line => {
+    if (line[0] === '#') {
+      const options = line.split(/ +/)
+      sparams.freq = options[1]
+      sparams.parameter = options[2]
+      sparams.format = options[3]
+      sparams.resistance = options[5]
+    } else if (line[0] !== '!' && line) {
+      // not a comment or empty line - set data
+      const data = line.split(/ +/)
+      console.log(data)
+    }
+  })
 }
