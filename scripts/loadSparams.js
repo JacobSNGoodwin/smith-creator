498bc937498046c865abe1f10ce8fdce4760f93e
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-04 15:51:26 -0700

    Parse options line of snp file

--- scripts/loadSparams.js
+++ scripts/loadSparams.js
@@ -14,19 +14,17 @@ const loadSparams = file => {
       let sParamData = []
       let textArray = reader.result.split('\n')
 
-      let options = null
       let optionsIndex = null
-      let dataIndex = null // line where data starts (could be comments betten)
+      let dataIndex = null // line where data starts
       for (let i = 0; i < textArray.length; i++) {
         if (textArray[i][0] === '#') {
           optionsIndex = i
-          options = textArray[i]
         }
         if (
-          optionsIndex &&
-          textArray[i] &&
-          i > optionsIndex &&
-          textArray[i][0] !== '!'
+          optionsIndex && // we've already found options
+          textArray[i] && // the line has text
+          i > optionsIndex && // we're past the options index
+          textArray[i][0] !== '!' // it's not a comment line (can be comment after options, eww)
         ) {
           // start of data which needs to be handled different for different
           // values of nPort
@@ -35,19 +33,10 @@ const loadSparams = file => {
         }
       }
 
-      const dataArray = textArray
-        .slice(dataIndex)
-        .join(' ')
-        .split(/\s+/)
+      // parse options into desired format
+      const options = parseOptions(textArray[optionsIndex])
 
-      for (let i = 0; i < dataArray.length; i += 1 + 2 * nPorts) {
-        const dataAtFreq = dataArray.slice(i, i + 1 + 2 * nPorts)
-        if (dataAtFreq.length !== i + 1 + 2 * nPorts) {
-          break
-        }
-        const freqPoint = dataAtFreq[0]
-        for (let j = 1; j < dataAtFreq.length; j++) {}
-      }
+      console.log(options)
 
       resolve(sparams)
     }
@@ -56,6 +45,54 @@ const loadSparams = file => {
       reject('Failed to load file', error)
     }
   })
+}
+
+// parseOptions takes the options string from the touchstone file
+// returns an object with the options parameters
+const parseOptions = optionsString => {
+  // set defaults based on spec
+  const options = {
+    freqUnit: 'GHz',
+    paramType: 'S',
+    format: 'MA',
+    Z0: 50
+  }
+
+  const optionsArray = optionsString
+    .trim()
+    .toUpperCase()
+    .split(/ +/)
+
+  while (optionsArray.length > 0) {
+    // shift elements out of array as they're found
+    const option = optionsArray[0]
+    if (option === '#') {
+      optionsArray.shift()
+    } else if (
+      option === 'GHZ' ||
+      option === 'MHZ' ||
+      option === 'KHZ' ||
+      option === 'HZ'
+    ) {
+      options.freqUnit = optionsArray.shift()
+    } else if (
+      option === 'S' ||
+      option === 'Y' ||
+      option === 'Z' ||
+      option === 'H' ||
+      option === 'G'
+    ) {
+      options.paramType = optionsArray.shift()
+    } else if (option === 'DB' || option === 'MA' || option === 'RI') {
+      options.format = optionsArray.shift()
+    } else if (option === 'R') {
+      optionsArray.shift()
+      options.Z0 = +optionsArray.shift()
+    } else {
+      optionsArray.shift()
+    }
+  }
+  return options
 }
 
 export default loadSparams
