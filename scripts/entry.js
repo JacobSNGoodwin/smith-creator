e940a3f1ce220aefa9cc451720aeef2f5dde5d20
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-11 17:17:35 -0700

    Integrate npm rf-network class

--- scripts/entry.js
+++ scripts/entry.js
@@ -1,5 +1,5 @@
 // import { loadSparams } from './loadSparams'
-import { loadSparams } from 'rf-network'
+import Network from 'rf-network'
 import { SmithChart } from './smith'
 
 let smithChart = new SmithChart('#smith-container')
@@ -7,5 +7,17 @@ smithChart.update()
 
 const fileInput = document.getElementById('fileInput')
 fileInput.addEventListener('change', event => {
-  loadSparams(event)
+  const file = event.target.files[0]
+  const reader = new FileReader()
+
+  reader.onload = e => {
+    const test = new Network(e.target.result)
+    console.log(test.touchstoneText, test.refImpedance)
+  }
+
+  reader.onerror = e => {
+    console.log('Error: ', e)
+  }
+
+  reader.readAsText(file)
 })
